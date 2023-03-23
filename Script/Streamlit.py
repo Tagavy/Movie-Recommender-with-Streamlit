edbf2e65d7a027afb922704bba46a8e0deb57ab3
Tagavy <115953254+Tagavy@users.noreply.github.com>
2023-03-23 13:02:28 +0100

    Update Streamlit.py

--- Script/Streamlit.py
+++ Script/Streamlit.py
@@ -91,7 +91,7 @@ for i in pop_rec(genre_inp, year_inp, num_inp1).title:
         continue       
 #posters_list 
 
-columns1 = st.columns(len(titles_list1))
+columns1 = st.columns(len(posters_list1))
 for a, i, j in zip (columns1, posters_list1, titles_list1):
     with a:
         st.image(i, j, 100)
